2ed6696a05b48a2520d7f44545a88eee4f37d00c
Jean-Luc Martin <jl.martin1990@gmail.com>
2016-05-13 15:59:05 -0400

    log repsonse from twitch alerts

--- index.js
+++ index.js
@@ -62,7 +62,21 @@ app.get('/donation', (req, res) => {
     }
   };
 
-  var request = new http.ClientRequest(options);
+  callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+    });
+  }
+
+  var request = new http.ClientRequest(options, callback);
   request.end(data);
 
   res.send(200);
